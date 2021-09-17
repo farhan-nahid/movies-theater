09532e219cfe5d0a96c78af6952188ec5457988c
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-17 15:04:18 +0600

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# movie-theater
+## Movies Theater
+
+---
+
+> This is a simple landing page. On this website, we can search for any movies & filter the movies.
+
+---
+
+##
+
+<img src="https://i.ibb.co/TPD8Z96/Movies-Theater.png" alt="Movies-Theater" border="0">
+
+---
+
+## Live Link
+
+[Link](https://movies-theater.vercel.app/)
+
+---
+
+## API Link
+
+> Display 1st page movies
+
+```json
+https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={key}&page=1
+```
+
+> Search By Leagues Team Name
+
+```json
+https://api.themoviedb.org/3/search/movie?api_key=${key}&query={search-text}
+```
+
+> filter
+
+```json
+https://api.themoviedb.org/3/genre/movie/list?api_key={key}
+```
+
+## Technology
+
+- [x] HTML5
+- [x] CSS3
+- [x] JAVASCRIPT
+- [x] API
+
+---
+
+## Tools
+
+- [x] VS CODE
+- [x] GITHUB
+- [x] GIT
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,64 @@
+// all APIs
+
+const baseURL = "https://api.themoviedb.org/3/";
 const key = "3f858ae293cd40cb5f9a355975f2782c";
-const apiURL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${key}&page=1`;
+const apiURL = `${baseURL}discover/movie?sort_by=popularity.desc&api_key=${key}&page=1`;
+const searchURL = `${baseURL}search/movie?api_key=${key}&query="`;
+const filterURL = `${baseURL}genre/movie/list?api_key=${key}`;
 const imgPATH = "https://image.tmdb.org/t/p/w1280";
-const searchAPI = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query="`;
+
+// html elements
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const moviesContainer = document.getElementById("movie__container");
+const category = document.getElementById("category");
 
-// get the movies
-getMovies(apiURL);
+const fetchGenres = async () => {
+  category.textContent = "";
+  const res = await fetch(filterURL);
+  const data = await res.json();
+  showTheGenres(data.genres);
+};
 
-async function getMovies(url) {
+const getMovies = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
-
   showMovies(data.results);
-}
+};
+
+// show the genres & movies
+
+fetchGenres();
+getMovies(apiURL);
+
+// filer movies
+
+const showTheGenres = (movies) => {
+  movies.forEach((movie) => {
+    const { id, name } = movie;
+    const categoryItem = document.createElement("div");
+    categoryItem.setAttribute("class", "category__item");
+    categoryItem.innerText = name;
+    categoryItem.addEventListener("click", () => {
+      const url = apiURL + "&with_genres=" + id + ",";
+      console.log(url);
+    });
+    category.appendChild(categoryItem);
+  });
+};
+
+// show movies function
 
 const showMovies = (movies) => {
   moviesContainer.textContent = "";
   movies.forEach((movie) => {
-    console.log(movie);
     const { title, poster_path, vote_average, overview } = movie;
-    const img = `'${imgPATH}${poster_path}'`;
-    console.log(img);
     const movieDiv = document.createElement("div");
     movieDiv.setAttribute("class", "movie");
     movieDiv.innerHTML = `
-    
           <img
-            src="${imgPATH}${poster_path}"
+            src="${imgPATH + poster_path}"
             alt="${title}"
           />
           <div class="movie__info">
@@ -40,28 +69,31 @@ const showMovies = (movies) => {
             <h3>overview</h3>
             <p>${overview.slice(0, 200)}</p>
           </div>
-    
-    `;
+        `;
     moviesContainer.appendChild(movieDiv);
   });
 };
 
+// add class
+
 const getClassByVote = (vote) => {
-  if (vote >= 9) {
+  if (vote > 8.5) {
     return "green";
-  } else if (vote >= 7) {
+  } else if (vote >= 6) {
     return "orange";
   } else {
     return "red";
   }
 };
 
+// search function
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchText = search.value;
   if (searchText && searchText !== "") {
-    getMovies(searchAPI + searchText);
+    getMovies(searchURL + searchText);
 
     search.value = "";
   } else {
